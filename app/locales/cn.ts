2c9d4fb63bd48c4eff3768685e3f245b3b069a3b
ten2net <ten2net@163.com>
2023-05-16 17:35:01 +0800

    修改面具为智能体

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -32,7 +32,7 @@ const cn = {
     Send: "发送",
     Config: {
       Reset: "重置默认",
-      SaveAs: "另存为面具",
+      SaveAs: "另存为智能体",
     },
   },
   Export: {
@@ -105,8 +105,8 @@ const cn = {
       SubTitle: "在预览气泡中预览 Markdown 内容",
     },
     Mask: {
-      Title: "面具启动页",
-      SubTitle: "新建问诊时，展示面具启动页",
+      Title: "智能体启动页",
+      SubTitle: "新建问诊时，展示智能体启动页",
     },
     Prompt: {
       Disable: {
@@ -194,11 +194,11 @@ const cn = {
     Name: "插件",
   },
   Mask: {
-    Name: "面具",
+    Name: "智能体",
     Page: {
-      Title: "预设角色面具",
+      Title: "预设角色智能体",
       SubTitle: (count: number) => `${count} 个预设角色定义`,
-      Search: "搜索角色面具",
+      Search: "搜索角色智能体",
       Create: "新建",
     },
     Item: {
@@ -211,7 +211,7 @@ const cn = {
     },
     EditModal: {
       Title: (readonly: boolean) =>
-        `编辑预设面具 ${readonly ? "（只读）" : ""}`,
+        `编辑预设智能体 ${readonly ? "（只读）" : ""}`,
       Download: "下载预设",
       Clone: "克隆预设",
     },
@@ -225,8 +225,8 @@ const cn = {
     Skip: "直接开始",
     NotShow: "不再展示",
     ConfirmNoShow: "确认禁用？禁用后可以随时在设置中重新启用。",
-    Title: "挑选一个面具",
-    SubTitle: "现在开始，与面具背后的灵魂思维碰撞",
+    Title: "挑选一个智能体",
+    SubTitle: "现在开始，与智能体背后的灵魂思维碰撞",
     More: "查看全部",
   },
 
--- app/locales/tw.ts
+++ app/locales/tw.ts
@@ -32,7 +32,7 @@ const tw: LocaleType = {
     Send: "發送",
     Config: {
       Reset: "重置默认",
-      SaveAs: "另存为面具",
+      SaveAs: "另存为智能体",
     },
   },
   Export: {
@@ -104,8 +104,8 @@ const tw: LocaleType = {
       SubTitle: "在预览气泡中预览 Markdown 内容",
     },
     Mask: {
-      Title: "面具启动页",
-      SubTitle: "新建聊天时，展示面具启动页",
+      Title: "智能体启动页",
+      SubTitle: "新建聊天时，展示智能体启动页",
     },
     Prompt: {
       Disable: {
@@ -190,11 +190,11 @@ const tw: LocaleType = {
   },
   Plugin: { Name: "插件" },
   Mask: {
-    Name: "面具",
+    Name: "智能体",
     Page: {
-      Title: "预设角色面具",
+      Title: "预设角色智能体",
       SubTitle: (count: number) => `${count} 个预设角色定义`,
-      Search: "搜索角色面具",
+      Search: "搜索角色智能体",
       Create: "新建",
     },
     Item: {
@@ -207,7 +207,7 @@ const tw: LocaleType = {
     },
     EditModal: {
       Title: (readonly: boolean) =>
-        `编辑预设面具 ${readonly ? "（只读）" : ""}`,
+        `编辑预设智能体 ${readonly ? "（只读）" : ""}`,
       Download: "下载预设",
       Clone: "克隆预设",
     },
@@ -219,8 +219,8 @@ const tw: LocaleType = {
   NewChat: {
     Return: "返回",
     Skip: "跳过",
-    Title: "挑选一个面具",
-    SubTitle: "现在开始，与面具背后的灵魂思维碰撞",
+    Title: "挑选一个智能体",
+    SubTitle: "现在开始，与智能体背后的灵魂思维碰撞",
     More: "搜索更多",
     NotShow: "不再展示",
     ConfirmNoShow: "确认禁用？禁用后可以随时在设置中重新启用。",
